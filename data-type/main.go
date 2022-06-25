23c1c8da14847685264d5adbf7bc41525ef6ec50
YadBro <ncada708@gmail.com>
2022-06-25 21:03:24 +0700

    tipe data

---
+++ data-type/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// NUMBERS ===============
+	fmt.Println("Ini adalah tipe data number")
+	// integer
+	fmt.Println(1)
+	// floating point number (decimal)
+	fmt.Println(1.2)
+	// NUMBERS ===============
+
+	// STRING ============
+	fmt.Println("Ini adalah tipe data string")
+	fmt.Println("Hello World")
+	// STRING ============
+
+	// BOOLEAN ==========
+	fmt.Println("Ini adalah tipe data boolean")
+	fmt.Println(true, false)
+	// BOOLEAN ==========
+}
