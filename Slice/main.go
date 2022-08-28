08ea60c771c6d3474ebb29f0da5696e9327e6151
YadBro <ncada708@gmail.com>
2022-08-28 21:12:11 +0700

    Slice - update

--- Slice/main.go
+++ Slice/main.go
@@ -46,9 +46,9 @@ func main() {
 	fmt.Println("Data Type Months :", reflect.TypeOf(months))
 	fmt.Println("Data Type Days :", reflect.TypeOf(days))
 
-	// Manipulation 1
+	// Manipulation Months
 	fmt.Println("'\n\n'")
-	fmt.Println("================ Manipulation 1 ================")
+	fmt.Println("================ Manipulation Months 1 ================")
 	sliceMonths1 := months[5:7]
 	fmt.Println("sliceMonths1 :", sliceMonths1)
 
@@ -56,15 +56,34 @@ func main() {
 	fmt.Println("Length from sliceMonths1 :", len(sliceMonths1))
 	fmt.Println("Capacity from sliceMonths1 :", cap(sliceMonths1))
 
-	// // Manipulation 2
-	// fmt.Println("================ Manipulation 2 ================")
-	// sliceMonths2 := append(sliceMonths1, "")
-	// fmt.Println("sliceMonths2 :", sliceMonths2)
+	fmt.Println("'\n'")
+	fmt.Println("================ Manipulation Months 1 ================")
+	sliceMonths1 = append(sliceMonths1, "Abang jago")
+	fmt.Println("sliceMonths1 :", sliceMonths1)
+	fmt.Println("Months :", months)
 
-	// // Inspect sliceMonths2
-	// fmt.Println("Length from sliceMonths2 :", len(sliceMonths2))
-	// fmt.Println("Capacity from sliceMonths2 :", cap(sliceMonths2))
+	// Inspect sliceMonths1
+	fmt.Println("Length from sliceMonths1 :", len(sliceMonths1))
+	fmt.Println("Capacity from sliceMonths1 :", cap(sliceMonths1))
 
+	// Manipulation Days
+	fmt.Println("'\n\n'")
+	fmt.Println("================ Manipulation Days 1 ================")
+	sliceDays := days[6:7]
+	fmt.Println("sliceDays :", sliceDays)
+
+	// Inspect sliceDays
+	fmt.Println("Length from sliceDays :", len(sliceDays))
+	fmt.Println("Capacity from sliceDays :", cap(sliceDays))
+
+	// Manipulation Days2
+	fmt.Println("'\n'")
+	fmt.Println("================ Manipulation Days 2 ================")
+	sliceDays = append(sliceDays, "New day!")
+	fmt.Println("sliceDays :", sliceDays)
+	fmt.Println("days :", days)
+
+	fmt.Println("'\n\n'")
 	var myKTP noKTP = "12321341214213434"
 	fmt.Println("MyKTP :", myKTP)
 
