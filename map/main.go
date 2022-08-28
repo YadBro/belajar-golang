f0086d4d435572f03e8cd88441d2827742ef4dff
YadBro <ncada708@gmail.com>
2022-08-28 22:11:43 +0700

    update

--- map/main.go
+++ map/main.go
@@ -23,7 +23,7 @@ func main() {
 	fmt.Println(people["name"])
 
 	students := map[int]map[string]string{
-		0: map[string]string{ // versi panjang
+		0: map[string]string{ // ini dry / redundant ikuti dry principles!
 			"name":    "yadi",
 			"address": "cimahi",
 		},
@@ -40,6 +40,26 @@ func main() {
 	// Results
 	fmt.Println(students)
 
+	// Add new map data
+	students[3] = map[string]string{
+		"name":    "saipul",
+		"address": "malang",
+	}
+
+	// Results
+	fmt.Println(students)
+
 	// How to access
-	fmt.Println("name :", students[0]["name"], "\naddress :", students[0]["address"])
+	fmt.Println("name :", students[3]["name"], "\naddress :", students[3]["address"])
+
+	// Read all
+	fmt.Print("\n\n=============== ALL ===============\n")
+	for key, value := range students {
+		fmt.Println("key: ", key)
+		fmt.Println("value: ", value)
+		fmt.Println("students[key]: ", students[key])
+		fmt.Println("students[key][name]: ", students[key]["name"])
+		fmt.Println("students[key][address]: ", students[key]["address"])
+		fmt.Println("name :", value["name"], "\naddress :", value["address"], "\n''")
+	}
 }
