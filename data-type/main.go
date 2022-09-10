0f33129622b3ec964f2ef1543eedf92f07c430fe
YadBro <ncada708@gmail.com>
2022-09-10 10:19:53 +0700

    update data-type

--- data-type/main.go
+++ data-type/main.go
@@ -7,6 +7,31 @@ func main() {
 	fmt.Println("Ini adalah tipe data number")
 	// integer
 	fmt.Println(1)
+
+	// integer8 -128 ~ 127
+	fmt.Println(int8(127))
+
+	// integer16 -32768 ~ 32767
+	fmt.Println(int16(32767))
+
+	// integer32 -2147483648 ~ 2147483647
+	fmt.Println(int32(2147483647))
+
+	// integer64 -9223372036854775808  ~ 9223372036854775807
+	fmt.Println(int64(9223372036854775807))
+
+	// unsignedInteger8 0 ~ 127
+	fmt.Println(uint8(127))
+
+	// unsignedInteger16 0 ~ 32767
+	fmt.Println(uint16(32767))
+
+	// unsignedInteger32 0 ~ 2147483647
+	fmt.Println(uint32(2147483647))
+
+	// unsignedInteger64 0 ~ 9223372036854775807
+	fmt.Println(uint64(9223372036854775807))
+
 	// floating point number (decimal)
 	fmt.Println(1.2)
 	// NUMBERS ===============
