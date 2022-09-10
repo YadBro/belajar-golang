c5a84e4ea5404d5e0a4ad68035d57c8186728ffa
YadBro <ncada708@gmail.com>
2022-09-10 10:29:14 +0700

    update type-declaration

--- Type-Declarations/main.go
+++ Type-Declarations/main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	type noKTP string
+	type Married bool
 
 	var myKTP noKTP = "12321341214213434"
+	var myWife Married = true
 	fmt.Println(myKTP)
+	fmt.Print(myWife)
 }
