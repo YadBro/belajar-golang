9f73b6375e6edb49a0565df463311a7356e31a2c
YadBro <ncada708@gmail.com>
2022-09-11 19:39:15 +0700

    updatE

--- database/connection.go
+++ database/connection.go
@@ -2,6 +2,8 @@ package database
 
 var connection string
 
+// Init function adalah sebuah function yang akan di eksekusi pertama kali ketika ada yang mengimportnya,
+// sama seperti dalam OOP yaitu constructor.
 func init() {
 	connection = "MySQL"
 }
