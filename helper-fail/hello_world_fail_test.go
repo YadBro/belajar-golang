6a761f3bbc43715cf7d280d3533ca34580fa2419
YadBro <ncada708@gmail.com>
2022-09-17 15:25:22 +0700

    update (t.Error and t.Fatal)

--- helper-fail/hello_world_fail_test.go
+++ helper-fail/hello_world_fail_test.go
@@ -25,3 +25,23 @@ func TestHelloWorldApriyadi(t *testing.T) {
 
 	fmt.Println("TestHelloWorld Apriyadi done")
 }
+
+func TestHelloWorldError(t *testing.T) {
+	result := HelloWorldFail("Yadi")
+
+	if result != "Hello Yadi" {
+		t.Error("Result must be 'Hello Yadi'")
+	}
+
+	fmt.Println("TestHelloWorldError Yadi done")
+}
+
+func TestHelloWorldFail(t *testing.T) {
+	result := HelloWorldFail("Apriyadi")
+
+	if result != "Hello Apriyadi" {
+		t.Fatal("Result must be 'Hello Apriyadi'")
+	}
+
+	fmt.Println("TestHelloWorldError Apriyadi done")
+}
