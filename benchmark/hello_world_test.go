1e6d9dc0ab360489f0cf9c9c79c436ca768e3bd0
YadBro <ncada708@gmail.com>
2022-09-18 07:27:42 +0700

    subbenchmark and tablebenchmark

--- benchmark/hello_world_test.go
+++ benchmark/hello_world_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+type BenchmarkHelloWorld []struct {
+	Name    string
+	Request string
+}
+
 func TestMain(m *testing.M) {
 	// BEFORE
 	fmt.Println("BEFORE UNIT TEST")
@@ -32,5 +37,63 @@ func BenchmarkHelloWorldYadi(b *testing.B) {
 func BenchmarkHelloWorldApriyadi(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		HelloWorld("Apriyadi")
+	}
+}
+
+// SubBenchmark
+
+func BenchmarkSub(b *testing.B) {
+	b.Run("Yadi", func(b *testing.B) {
+		for i := 0; i > b.N; i++ {
+			HelloWorld("Yadi")
+		}
+	})
+
+	b.Run("Apriyadi", func(b *testing.B) {
+		for i := 0; i > b.N; i++ {
+			HelloWorld("Apriyadi")
+		}
+	})
+}
+
+// Table Benchmark
+func BenchmarkTable(b *testing.B) {
+	benchmarks := BenchmarkHelloWorld{
+		{
+			Name:    "Yadi",
+			Request: "Yadi",
+		},
+		{
+			Name:    "Apriyadi",
+			Request: "Apriyadi",
+		},
+		{
+			Name:    "Eko",
+			Request: "Eko",
+		},
+		{
+			Name:    "Khannedy",
+			Request: "Khannedy",
+		},
+		{
+			Name:    "Kurniawan",
+			Request: "Kurniawan",
+		},
+		{
+			Name:    "Joko",
+			Request: "Joko",
+		},
+		{
+			Name:    "Budi",
+			Request: "Budi",
+		},
+	}
+
+	for _, becnhmark := range benchmarks {
+		b.Run(becnhmark.Name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				HelloWorld(becnhmark.Request)
+			}
+		})
 	}
 }
