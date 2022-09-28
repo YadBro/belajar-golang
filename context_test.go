658c903cce43e8772bd32c05edebef3d30a52fd8
YadBro <ncada708@gmail.com>
2022-09-29 03:31:17 +0700

    context with timeout

--- context_test.go
+++ context_test.go
@@ -55,6 +55,7 @@ func CreateCounter(ctx context.Context) chan int {
 			default:
 				destination <- counter
 				counter++
+				time.Sleep(1 * time.Second) // Slow simulation
 			}
 		}
 	}()
@@ -79,6 +80,25 @@ func TestContextWithCancel(t *testing.T) {
 	}
 
 	cancel()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel() // Mengirim sinyal cancel ke context
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
 
 	time.Sleep(2 * time.Second)
 
